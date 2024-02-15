bf0bb236887b865efe7ff22ad0b73c81e4b651ad
John <john.t.singer@gmail.com>
2024-02-15 11:05:08 +0100

    add verbs model

--- training/verbs/models.py
+++ training/verbs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Verb(models.Model):
+    infinitive = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    simple_past = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    past_participle = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    translation = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    def __str__(self):
+        return (
+            f'{self.infinitive} {self.simple_past}'
+            f'{self.past_participle} {self.translation}'
+        )
