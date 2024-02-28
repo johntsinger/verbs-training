4b9f4d2eeee67456b4ae1a8fb144da5ea13e7577
John <john.t.singer@gmail.com>
2024-02-28 15:46:23 +0100

    Remove tables from Profile, remove UserTable

--- training/profiles/models.py
+++ training/profiles/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from verbs.models import Verb
-from tables.models import Table
 
 
 User = get_user_model()
@@ -17,11 +16,6 @@ class Profile(models.Model):
         through='UserVerb',
         related_name='profiles'
     )
-    tables = models.ManyToManyField(
-        to=Table,
-        through='UserTable',
-        related_name='profiles'
-    )
 
     def __str__(self):
         return self.user.username
@@ -31,31 +25,10 @@ class UserVerb(models.Model):
     profile = models.ForeignKey(
         to=Profile,
         on_delete=models.CASCADE,
-        related_name='user_verbs'
     )
     verb = models.ForeignKey(
         to=Verb,
         on_delete=models.CASCADE,
-        related_name='user_verbs'
-    )
-    done = models.BooleanField(
-        default=False
-    )
-    success = models.BooleanField(
-        default=False
-    )
-
-
-class UserTable(models.Model):
-    profile = models.ForeignKey(
-        to=Profile,
-        on_delete=models.CASCADE,
-        related_name='user_tables'
-    )
-    table = models.ForeignKey(
-        to=Table,
-        on_delete=models.CASCADE,
-        related_name='user_tables'
     )
     done = models.BooleanField(
         default=False
