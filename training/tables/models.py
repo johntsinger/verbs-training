7503c747dbe4d0e3798596b99fa7947128511b9a
John <john.t.singer@gmail.com>
2024-02-28 16:24:11 +0100

    Add AbstractTable, DefaultTable and UserTable, update TableVerb

--- training/tables/models.py
+++ training/tables/models.py
@@ -1,32 +1,85 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 from verbs.models import Verb
+from profiles.models import Profile
 
 
-class Table(models.Model):
+class AbstractTable(models.Model):
     name = models.fields.CharField(
         max_length=30
     )
-    verbs = models.ManyToManyField(
-        Verb,
-        through='TableVerb',
-        related_name='tables'
-    )
-    default = models.fields.BooleanField(
-        default=False
-    )
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
 
 
+class DefaultTable(AbstractTable):
+    verbs = models.ManyToManyField(
+        Verb,
+        related_name='defaut_tables'
+    )
+    profile = None
+    __original_name = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_name = self.name
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            if self.__original_name != self.name:
+                UserTable.objects.filter(
+                    name=self.__original_name
+                ).update(
+                    name=self.name
+                )
+                self.__original_name = self.name
+        else:
+            user_tables = [
+                UserTable(
+                    name=self.name,
+                    profile=profile
+                )
+                for profile in Profile.objects.all()
+            ]
+            UserTable.objects.bulk_create(user_tables)
+        super().save(*args, **kwargs)
+
+
+class UserTable(AbstractTable):
+    verbs = models.ManyToManyField(
+        Verb,
+        through='TableVerb',
+        related_name='user_tables'
+    )
+    profile = models.ForeignKey(
+        to=Profile,
+        on_delete=models.CASCADE,
+        related_name='user_tables',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['name', 'profile'],
+                name='unique table name'
+            )
+        ]
+
+
 class TableVerb(models.Model):
     table = models.ForeignKey(
-        to=Table,
-        on_delete=models.CASCADE
+        to=UserTable,
+        on_delete=models.CASCADE,
     )
     verb = models.ForeignKey(
         to=Verb,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     done = models.BooleanField(
         default=False
