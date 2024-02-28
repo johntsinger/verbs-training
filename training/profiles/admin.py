6bfbbcf8d21b6a40b781587dc609713680d8f908
John <john.t.singer@gmail.com>
2024-02-28 15:40:46 +0100

    add ProfileAdminForm and ProfileAdmin

--- training/profiles/admin.py
+++ training/profiles/admin.py
@@ -1,7 +1,49 @@
 from django.contrib import admin
-from profiles.models import Profile, UserVerb, UserTable
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django import forms
+from profiles.models import Profile, UserVerb
+from tables.models import UserTable
 
 
-admin.site.register(Profile)
+class ProfileAdminForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'user',
+            'tables'
+        ]
+
+    tables = forms.ModelMultipleChoiceField(
+        queryset=UserTable.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(
+            verbose_name='Tables',
+            is_stacked=False
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['tables'].initial = self.instance.user_tables.all()
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        if commit:
+            profile.save()
+
+        if profile.pk:
+            profile.tables.set(
+                self.cleaned_data['tables']
+            )
+            self.save_m2m()
+
+        return profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileAdminForm
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(UserVerb)
-admin.site.register(UserTable)
