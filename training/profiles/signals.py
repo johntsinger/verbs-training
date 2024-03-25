d63f099fdbfcd49929d76275d298c008b0447727
John <john.t.singer@gmail.com>
2024-03-25 23:54:03 +0100

    remove create_and_save_user_default_table

--- training/profiles/signals.py
+++ training/profiles/signals.py
@@ -2,7 +2,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from profiles.models import Profile
-from tables.models import DefaultTable, UserTable
 
 
 User = get_user_model()
@@ -25,21 +24,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 )
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-
-@receiver(
-    post_save,
-    sender=Profile,
-    dispatch_uid='create_and_save_user_default_tables'
-)
-def create_and_save_user_default_table(sender, instance, created, **kwargs):
-    if created:
-        default_tables = DefaultTable.objects.prefetch_related('verbs')
-        for default_table in default_tables:
-            table = UserTable.objects.create(
-                name=default_table.name,
-                profile=instance,
-            )
-            table.verbs.set(
-                default_table.verbs.all()
-            )
