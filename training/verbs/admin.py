60dcd7d5da9d78a35ad5a84b5c5aceec6029bad3
John <john.t.singer@gmail.com>
2024-03-25 23:59:03 +0100

    add VerbAdmin

--- training/verbs/admin.py
+++ training/verbs/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from verbs.models import Verb
 
 
-admin.site.register(Verb)
+class VerbAdmin(admin.ModelAdmin):
+    list_display = [
+        'infinitive',
+        'simple_past',
+        'past_participle',
+        'translation'
+    ]
+    ordering = ('infinitive',)
+
+
+admin.site.register(Verb, VerbAdmin)
