5b668b2118561af96dde8ed8f9f7c6f490d2c6fd
John <john.t.singer@gmail.com>
2024-03-25 23:52:16 +0100

    add uuid, add created_at and updated_at, remove m2m to DefaultTable, remove Result model

--- training/profiles/models.py
+++ training/profiles/models.py
@@ -1,53 +1,28 @@
+import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
-from verbs.models import Verb
 
 
 User = get_user_model()
 
 
 class Profile(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        unique=True,
+        editable=False,
+        default=uuid.uuid4
+    )
     user = models.OneToOneField(
         to=User,
         on_delete=models.CASCADE
     )
-    default_tables = models.ManyToManyField(
-        to='tables.DefaultTable',
-        related_name='profiles'
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
     )
 
     def __str__(self):
         return self.user.username
-
-
-class Result(models.Model):
-    profile = models.ForeignKey(
-        to=Profile,
-        on_delete=models.CASCADE,
-        related_name='results'
-    )
-    verb = models.ForeignKey(
-        to=Verb,
-        on_delete=models.CASCADE,
-        related_name='results'
-    )
-    table = models.ForeignKey(
-        to='tables.UserTable',
-        on_delete=models.CASCADE,
-        related_name='results',
-        null=True,
-        blank=True,
-    )
-    default_table = models.ForeignKey(
-        to='tables.DefaultTable',
-        on_delete=models.CASCADE,
-        related_name='results',
-        null=True,
-        blank=True,
-    )
-    done = models.BooleanField(
-        default=False
-    )
-    success = models.BooleanField(
-        default=False
-    )
