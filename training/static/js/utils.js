214793c7172c447ec755d113526a3fa6b7fd08d2
John <john.t.singer@gmail.com>
2024-04-19 12:44:53 +0200

    add function to close alert automatically after 5 seconds

--- training/static/js/utils.js
+++ training/static/js/utils.js
@@ -3,3 +3,9 @@ function moveCursorOnFocus(element) {
     element.value='';
     element.value=temp_value
 }
+
+window.setTimeout(function() {
+    $(".alert-auto-close").fadeTo(500, 0).slideUp(500, function(){
+        $(this).alert('close'); 
+    });
+}, 5000);
