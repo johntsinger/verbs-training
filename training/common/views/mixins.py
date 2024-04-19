626ec1f50ac976b74ad340077cf9fda4b792ed8e
John <john.t.singer@gmail.com>
2024-04-19 12:38:00 +0200

    add PreviousPageURLMixin

---
+++ training/common/views/mixins.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ImproperlyConfigured
+
+
+class PreviousPageURLMixin:
+    """Provide a previous page URL attribute and pass it to the context"""
+    previous_page_url = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['previous_page_url'] = self.get_return_url
+        return context
+
+    def get_return_url(self):
+        """Retrun the URL to redirect after clicking on 'return' button"""
+        if not self.previous_page_url:
+            raise ImproperlyConfigured(
+                'No URL to return to. Provide a return_url.'
+            )
+        return self.previous_page_url
