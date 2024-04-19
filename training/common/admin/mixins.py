173e45b70eeec0175e8a3218296aae06d607314f
John <john.t.singer@gmail.com>
2024-04-19 12:39:35 +0200

    update docstring

--- training/common/admin/mixins.py
+++ training/common/admin/mixins.py
@@ -1,7 +1,5 @@
 class GetReadOnlyFieldsMixin:
-    """
-    Hide read only fields on creation form but not on change form.
-    """
+    """Hide read only fields on creation form but not on change form."""
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # Editing
