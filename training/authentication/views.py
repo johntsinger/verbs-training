8182958b9d897ad4eb2394e0b201a2c836956f94
John <john.t.singer@gmail.com>
2024-04-19 12:38:51 +0200

    update views to use PreviousPageURLMixin

--- training/authentication/views.py
+++ training/authentication/views.py
@@ -3,16 +3,19 @@ from django.views.generic.edit import (
     CreateView
 )
 from django.urls import reverse_lazy
+from common.views.mixins import PreviousPageURLMixin
 from authentication.forms import (
     SignUpForm
 )
 
 
 class SignUpView(
+    PreviousPageURLMixin,
     SuccessMessageMixin,
     CreateView
 ):
     template_name = 'authentication/signup.html'
-    success_url = reverse_lazy('login')
     form_class = SignUpForm
     success_message = "Your account was created successfully"
+    success_url = reverse_lazy('login')
+    previous_page_url = reverse_lazy('login')
