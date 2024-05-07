04705c52fbefd8a6f3935a7a862a9073f9ad5608
John <john.t.singer@gmail.com>
2024-05-07 22:04:09 +0200

    add VerbListView

--- training/verbs/views.py
+++ training/verbs/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Subquery, OuterRef
+from django.views.generic.list import ListView
+from verbs.models import Verb
+from results.models import Result
 
-# Create your views here.
+
+class VerbListView(
+    ListView
+):
+    template_name = 'verbs/verbs.html'
+
+    def get_queryset(self):
+        """
+        Get all verbs and add is_success field based on the Result
+        subquery is_success field
+        """
+        if self.request.user.is_authenticated:
+            return Verb.objects.annotate(
+                is_success=Subquery(
+                    Result.objects.filter(
+                        verb=OuterRef('id'),
+                        profile=self.request.user.profile,
+                    ).order_by(
+                        '-updated_at'
+                    ).values('is_success')[:1]
+                )
+            )
+
+        return Verb.objects.all()
