9c611cbb55e07132337d3fdf9f07aaa393719035
John <john.t.singer@gmail.com>
2024-05-07 22:06:08 +0200

    update authentication urls

--- training/authentication/urls.py
+++ training/authentication/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from authentication import views
+
+
+"""
+from django.contrib.auth.views import LoginView
 from authentication.forms import LoginForm
+
+path(
+    'login/',
+    LoginView.as_view(
+        template_name='authentication/login.html',
+        authentication_form=LoginForm
+    ),
+    name='login'
+),
+"""
 
 
 urlpatterns = [
-    path(
-        'login/',
-        LoginView.as_view(
-            template_name='authentication/login.html',
-            authentication_form=LoginForm
-        ),
-        name='login'
-    ),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
