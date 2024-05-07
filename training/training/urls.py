447baf85f7dc2720becf426f488b6077ec25dcb5
John <john.t.singer@gmail.com>
2024-05-07 22:03:31 +0200

    add verbs urls

--- training/training/urls.py
+++ training/training/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
     path('account/', include('accounts.urls')),
+    path('verbs/', include('verbs.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
