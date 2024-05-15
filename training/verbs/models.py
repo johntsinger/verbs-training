2d439587badd789273f1d36f1b99948381b11400
John <john.t.singer@gmail.com>
2024-05-15 16:50:16 +0200

    add Example ans VerbGroup

--- training/verbs/models.py
+++ training/verbs/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class VerbGroup(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+
+
 class Verb(models.Model):
     infinitive = models.CharField(
         max_length=50,
@@ -18,9 +24,29 @@ class Verb(models.Model):
         max_length=50,
         blank=True
     )
+    group = models.ForeignKey(
+        to=VerbGroup,
+        on_delete=models.SET_NULL,
+        related_name='verbs',
+        null=True
+    )
 
     def __str__(self):
         return (
-            f'{self.infinitive} {self.simple_past} '
-            f'{self.past_participle} {self.translation}'
+            f"{self.infinitive} {self.simple_past} "
+            f"{self.past_participle} {self.translation}"
         )
+
+
+class Example(models.Model):
+    example = models.CharField(
+        max_length=255
+    )
+    translation = models.CharField(
+        max_length=255
+    )
+    verb = models.ForeignKey(
+        to=Verb,
+        on_delete=models.CASCADE,
+        related_name="examples"
+    )
