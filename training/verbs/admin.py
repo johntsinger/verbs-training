3676dbc2773ba44caeef3e99b9bbc0587deb8e33
John <john.t.singer@gmail.com>
2024-06-16 12:43:56 +0200

    add InfoInline, ExampleInline, InfoAdmin and ExampleAdmin

--- training/verbs/admin.py
+++ training/verbs/admin.py
@@ -1,7 +1,28 @@
 from django import forms
+from django.db import models
 from django.contrib import admin
 from django.core.exceptions import ValidationError
-from verbs.models import Verb
+from verbs.models import Verb, Info, Example
+
+
+class InfoInline(admin.StackedInline):
+    model = Info
+    extra = 0
+    formfield_overrides = {
+        models.CharField: {
+            'widget': forms.TextInput(attrs={"size": "100"})
+        },
+    }
+
+
+class ExampleInline(admin.StackedInline):
+    model = Example
+    extra = 0
+    formfield_overrides = {
+        models.CharField: {
+            'widget': forms.TextInput(attrs={"size": "100"})
+        },
+    }
 
 
 class VerbAdminForm(forms.ModelForm):
@@ -11,7 +32,8 @@ class VerbAdminForm(forms.ModelForm):
             'infinitive',
             'simple_past',
             'past_participle',
-            'translation'
+            'translation',
+            'similarity',
         ]
 
     def clean(self):
@@ -35,9 +57,39 @@ class VerbAdmin(admin.ModelAdmin):
         'infinitive',
         'simple_past',
         'past_participle',
-        'translation'
+        'translation',
+        'similarity',
+    ]
+    inlines = [
+        InfoInline,
+        ExampleInline
     ]
     ordering = ('infinitive',)
 
 
+class InfoAdmin(admin.ModelAdmin):
+    list_display = [
+        "get_verb",
+        "content"
+    ]
+    ordering = ('verb',)
+
+    def get_verb(self, obj):
+        return obj.verb.infinitive
+
+
+class ExampleAdmin(admin.ModelAdmin):
+    list_display = [
+        "get_verb",
+        "english",
+        "translation"
+    ]
+    ordering = ('verb',)
+
+    def get_verb(self, obj):
+        return obj.verb.infinitive
+
+
 admin.site.register(Verb, VerbAdmin)
+admin.site.register(Info, InfoAdmin)
+admin.site.register(Example, ExampleAdmin)
