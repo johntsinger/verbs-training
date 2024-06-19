a6d5461e01e8c17d215907f1404be97889d3a870
John <john.t.singer@gmail.com>
2024-06-19 09:11:10 +0200

    udpade admin models and inlines to add get_queryset and formfield_for_foreignkey to select related fields to avoid duplicated queries

--- training/tables/admin.py
+++ training/tables/admin.py
@@ -1,12 +1,17 @@
+from typing import Any
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django import forms
+from django.db.models.fields.related import ForeignKey
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from common.admin.mixins import GetReadOnlyFieldsMixin
 from tables.models import (
     DefaultTable,
     UserTable,
 )
 from verbs.models import Verb
+from profiles.models import Profile
 
 
 class TableAdminFormMixin:
@@ -93,6 +98,22 @@ class UserTableAdmin(
         'updated_at'
     )
     ordering = ('name',)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[UserTable]:
+        return UserTable.objects.select_related('profile__user')
+
+    def formfield_for_foreignkey(
+        self,
+        db_field: ForeignKey[Any],
+        request: HttpRequest | None,
+        **kwargs: Any
+    ) -> forms.ModelChoiceField | None:
+        # Select user to display foreignkey profile choices
+        # to avoid duplicated query because profile's str method
+        # access to user.username
+        if db_field.name == "profile":
+            kwargs["queryset"] = Profile.objects.select_related("user")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(DefaultTable, DefaultTableAdmin)
--- training/verbs/admin.py
+++ training/verbs/admin.py
@@ -2,6 +2,7 @@ from django import forms
 from django.db import models
 from django.contrib import admin
 from django.core.exceptions import ValidationError
+from django.http import HttpRequest
 from verbs.models import Verb, Info, Example
 
 
@@ -14,6 +15,10 @@ class InfoInline(admin.StackedInline):
         },
     }
 
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet[Info]:
+        qs = super().get_queryset(request)
+        return qs.select_related('verb')
+
 
 class ExampleInline(admin.StackedInline):
     model = Example
@@ -23,6 +28,10 @@ class ExampleInline(admin.StackedInline):
             'widget': forms.TextInput(attrs={"size": "100"})
         },
     }
+
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet[Example]:
+        qs = super().get_queryset(request)
+        return qs.select_related('verb')
 
 
 class VerbAdminForm(forms.ModelForm):
@@ -66,16 +75,28 @@ class VerbAdmin(admin.ModelAdmin):
     ]
     ordering = ('infinitive',)
 
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet[Verb]:
+        qs = super().get_queryset(request)
+        return qs.select_related("similarity")
+
 
 class InfoAdmin(admin.ModelAdmin):
     list_display = [
         "get_verb",
         "content"
     ]
-    ordering = ('verb',)
+    ordering = ("verb__infinitive",)
 
-    def get_verb(self, obj):
+    @admin.display(
+        ordering="verb__infinitive",
+        description='Verb'
+    )
+    def get_verb(self, obj: Info) -> str:
         return obj.verb.infinitive
+
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet[Info]:
+        qs = super().get_queryset(request)
+        return qs.select_related("verb")
 
 
 class ExampleAdmin(admin.ModelAdmin):
@@ -84,10 +105,18 @@ class ExampleAdmin(admin.ModelAdmin):
         "english",
         "translation"
     ]
-    ordering = ('verb',)
+    ordering = ("verb__infinitive",)
 
-    def get_verb(self, obj):
+    @admin.display(
+        ordering="verb__infinitive",
+        description='Verb'
+    )
+    def get_verb(self, obj: Example) -> str:
         return obj.verb.infinitive
+
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet[Example]:
+        qs = super().get_queryset(request)
+        return qs.select_related("verb")
 
 
 admin.site.register(Verb, VerbAdmin)
