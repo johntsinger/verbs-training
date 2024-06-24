164241290a20fd0eb6170fda03427fb6fd42615d
John <john.t.singer@gmail.com>
2024-06-24 23:41:14 +0200

    add table type and choices, update clean

--- training/tables/models.py
+++ training/tables/models.py
@@ -2,16 +2,28 @@ import uuid
 from django.db import models
 from django.db.models import Q
 from django.db.models.constraints import UniqueConstraint
+from django.core.exceptions import ValidationError
 from verbs.models import Verb
 from profiles.models import Profile
 
 
 class Table(models.Model):
+    DEFAULT_TABLE = "defaulttable"
+    USER_TABLE = "usertable"
+
+    CHOICES = [
+        (DEFAULT_TABLE, "Default Table"),
+        (USER_TABLE, "User Table")
+    ]
     id = models.UUIDField(
         primary_key=True,
         unique=True,
         editable=False,
         default=uuid.uuid4
+    )
+    type = models.CharField(
+        max_length=12,
+        choices=CHOICES
     )
     name = models.fields.CharField(
         max_length=30,
@@ -25,9 +37,6 @@ class Table(models.Model):
         on_delete=models.CASCADE,
         related_name="tables",
         null=True
-    )
-    is_default = models.BooleanField(
-        default=False
     )
     is_available = models.BooleanField(
         default=True
@@ -43,14 +52,14 @@ class Table(models.Model):
         constraints = [
             UniqueConstraint(
                 fields=['name'],
-                condition=Q(is_default=True),
+                condition=Q(type="defaulttable"),
                 name=(
                     'unique_default_table_name'
                 )
             ),
             UniqueConstraint(
                 fields=['name', 'owner'],
-                condition=Q(is_default=False),
+                condition=Q(type="usertable"),
                 name=(
                     'unique_table_name_per_profile'
                 )
@@ -58,123 +67,60 @@ class Table(models.Model):
         ]
 
     def __str__(self):
-        return (
-            f"{self.name} (default)"
-            if self.is_default
-            else f"{self.name} (user: {self.owner})"
-        )
+        return f"{self.name}"
 
 
 class DefaultTableManager(models.Manager):
     def get_queryset(self) -> models.QuerySet:
-        return super().get_queryset().filter(is_default=True)
+        return super().get_queryset().filter(type="defaulttable")
 
 
 class UserTableManager(models.Manager):
     def get_queryset(self) -> models.QuerySet:
-        return super().get_queryset().filter(is_default=False)
+        return super().get_queryset().filter(type="usertable")
 
 
 class DefaultTable(Table):
-
     objects = DefaultTableManager()
 
     class Meta:
         proxy = True
 
     def save(self, *args, **kwargs) -> None:
-        kwargs['is_default'] = True
-        kwargs['owner'] = None
+        self.type = self.DEFAULT_TABLE
+        self.owner = None
         return super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f"{self.name}"
+    def clean(self) -> None:
+        if DefaultTable.objects.filter(
+            name=self.name
+        ).exclude(
+            id=self.id
+        ):
+            raise ValidationError(
+                "Default table with this Name already exists.",
+            )
+        return super().clean()
 
 
 class UserTable(Table):
-
     objects = UserTableManager()
 
     class Meta:
         proxy = True
 
     def save(self, *args, **kwargs) -> None:
+        self.type = self.USER_TABLE
         return super().save(*args, **kwargs)
 
-    def __str__(self):
-        return f"{self.name} ({self.owner})"
-
-
-# class AbstractTable(models.Model):
-#     id = models.UUIDField(
-#         primary_key=True,
-#         unique=True,
-#         editable=False,
-#         default=uuid.uuid4
-#     )
-#     name = models.fields.CharField(
-#         max_length=30
-#     )
-#     verbs = models.ManyToManyField(
-#         to=Verb,
-#         related_name='%(class)s'
-#     )
-#     created_at = models.DateTimeField(
-#         auto_now_add=True
-#     )
-#     updated_at = models.DateTimeField(
-#         auto_now=True
-#     )
-
-#     class Meta:
-#         abstract = True
-
-#     def __str__(self):
-#         return self.name
-
-
-# class DefaultTable(AbstractTable):
-#     is_available = models.BooleanField(
-#         default=True
-#     )
-
-#     class Meta:
-#         constraints = [
-#             UniqueConstraint(
-#                 fields=['name'],
-#                 name=(
-#                     '%(app_label)s_%(class)s_'
-#                     'unique_name'
-#                 )
-#             )
-#         ]
-
-
-# class UserTable(AbstractTable):
-#     profile = models.ForeignKey(
-#         to=Profile,
-#         on_delete=models.CASCADE,
-#         related_name='usertables'
-#     )
-
-#     class Meta:
-#         constraints = [
-#             UniqueConstraint(
-#                 fields=['name', 'profile'],
-#                 name=(
-#                     '%(app_label)s_%(class)s_'
-#                     'unique_name_per_profile'
-#                 )
-#             )
-#         ]
-
-
-# def save(self, *args, **kwargs):
-#         # self._state.adding --> True (creation) or False (update)
-#         # Better than self.pk is None.
-#         created = self._state.adding
-#         super().save(*args, **kwargs)
-#         if created:
-#             self.profiles.set(
-#                 Profile.objects.all()
-#             )
+    def clean(self) -> None:
+        if UserTable.objects.filter(
+            name=self.name,
+            owner=self.owner
+        ).exclude(
+            id=self.id
+        ):
+            raise ValidationError(
+                "User table with this Name and Owner already exists.",
+            )
+        return super().clean()
