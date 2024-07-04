82d99f5ac86790366e9bf0bfa530ddb712dada76
John <john.t.singer@gmail.com>
2024-07-04 15:23:42 +0200

    remove unused import

--- training/accounts/forms.py
+++ training/accounts/forms.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import get_user_model, authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django import forms
 from django.utils.translation import gettext
