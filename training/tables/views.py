8c9fab387e6d0118fecb9c147775330afa211b18
John <john.t.singer@gmail.com>
2024-07-22 11:23:05 +0200

    add TableListView

--- training/tables/views.py
+++ training/tables/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Any
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.views.generic.list import ListView
+from tables.models import Table, DefaultTable, UserTable
 
-# Create your views here.
+
+class TableListView(LoginRequiredMixin, ListView):
+    model = Table
+    template_name = 'tables/tables_list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        return queryset.select_related('owner').prefetch_related('verbs').filter(
+            Q(owner=self.request.user.profile)
+            | Q(type='defaulttable')
+        )
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['default_tables'] = self.object_list.filter(
+            type='defaulttable'
+        )
+        context['user_tables'] = self.object_list.filter(
+            type='usertable'
+        )
+        return context
