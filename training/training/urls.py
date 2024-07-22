94958f6626dcce5fd183b5916ad990a5cbd101a1
John <john.t.singer@gmail.com>
2024-07-22 11:24:29 +0200

    add tables urls

--- training/training/urls.py
+++ training/training/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('authentication.urls')),
     path('account/', include('accounts.urls')),
     path('verbs/', include('verbs.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
+    path('tables/', include('tables.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
