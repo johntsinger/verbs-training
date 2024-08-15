98623e5209c5610235561e64b4e04c8cdcb668f2
John <john.t.singer@gmail.com>
2024-08-16 00:46:10 +0200

    add title mixin

--- training/common/views/mixins.py
+++ training/common/views/mixins.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 class PreviousPageURLMixin:
-    """Provide a previous page URL attribute and pass it to the context"""
+    """Provides a previous page URL attribute and pass it to the context"""
     previous_page_url = None
 
     def get_context_data(self, **kwargs):
@@ -11,9 +11,25 @@ class PreviousPageURLMixin:
         return context
 
     def get_previous_page_url(self):
-        """Retrun the URL to redirect after clicking on 'return' button"""
+        """Retruns the URL to redirect after clicking on 'return' button"""
         if not self.previous_page_url:
             raise ImproperlyConfigured(
                 'No URL to return to. Provide a previous_page_url.'
             )
         return self.previous_page_url
+
+
+class TitleMixin:
+    title = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
+
+    def get_title(self):
+        if not self.title:
+            raise ImproperlyConfigured(
+                'No title defined. Provide a title.'
+            )
+        return self.title
