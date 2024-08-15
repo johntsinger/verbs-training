71aff4515b74d552a3e56b7e1c3ff75e9ec815ef
John <john.t.singer@gmail.com>
2024-08-16 00:59:12 +0200

    add request factory mixin

---
+++ training/common/testcase/mixins.py
@@ -0,0 +1,23 @@
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.test import RequestFactory
+
+
+class RequestFactoryMixin:
+    """Set up request factory for each test case."""
+
+    def setUp(self):
+        super().setUp()
+        self.factory = RequestFactory()
+
+    def setUp_session(self, request):
+        """Set up session if request needs SessionMiddleware."""
+        setattr(request, 'session', {})
+
+    def setUp_messages(self, request):
+        """Set up messages if request needs MessageMiddleware."""
+
+        # message middleware requires session middleware
+        self.setUp_session(request)
+
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
