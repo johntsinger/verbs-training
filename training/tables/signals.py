5b0916bee18b821586b306b2a4454f448eece0b0
John <john.t.singer@gmail.com>
2024-08-16 00:52:26 +0200

    remove signals.py no signals used

--- training/tables/signals.py
+++
@@ -1,6 +0,0 @@
-from django.db import transaction
-from django.db.models.signals import m2m_changed, post_delete
-from django.dispatch import receiver
-from tables.models import DefaultTable, UserTable
-
-
