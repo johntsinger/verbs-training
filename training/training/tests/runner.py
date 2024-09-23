d5f22ef5a3f2ddf9fee467dae7695e527667c2b4
John <john.t.singer@gmail.com>
2024-09-23 12:20:48 +0200

    add docstrings

--- training/training/tests/runner.py
+++ training/training/tests/runner.py
@@ -5,9 +5,11 @@ from django.conf import settings
 
 class MyTestRunner(DiscoverRunner):
     def setup_test_environment(self, **kwargs: Any) -> None:
+        """Set IS_TEST settings to True during tests."""
         super(MyTestRunner, self).setup_test_environment(**kwargs)
         settings.IS_TEST = True
 
     def teardown_test_environment(self, **kwargs: Any) -> None:
+        """Set IS_TEST settings to False after testing is complete."""
         settings.IS_TEST = False
         super().teardown_test_environment(**kwargs)
