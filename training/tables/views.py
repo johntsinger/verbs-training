4729d9765959d4b8bfd62b3ee9fee6c229a7551f
John <john.t.singer@gmail.com>
2024-10-05 15:13:06 +0200

    Annotates verbs in tables with the corresponding is_success value

--- training/tables/views.py
+++ training/tables/views.py
@@ -1,12 +1,12 @@
-from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
-from django.db.models.query import QuerySet
+from django.db.models import Q, Prefetch, Subquery, OuterRef
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.list import ListView
 
 from common.views.mixins import TitleMixin
-from tables.models import Table
+from tables.models import Table, DefaultTable, UserTable
+from results.models import Result
+from verbs.models import Verb
 
 
 class TableListView(
@@ -16,25 +16,67 @@ class TableListView(
 ):
     model = Table
     template_name = 'tables/tables_list.html'
-    title = _('All tables')
+    title = _('Tables')
 
-    def get_queryset(self) -> QuerySet[Any]:
-        queryset = super().get_queryset()
-        return queryset.select_related(
-            'owner'
-        ).prefetch_related(
-            'verbs'
-        ).filter(
+    def get_queryset(self):
+        # Subquery to get is_success for each Verb
+        result_subquery = Result.objects.filter(
+            profile=self.request.user.profile,
+            table=OuterRef('tables'),
+            verb=OuterRef('pk')
+        ).values('is_success')[:1]
+
+        # Prefetch related verbs with the annotated is_success
+        verbs_prefetch = Prefetch(
+            'verbs',
+            queryset=Verb.objects.annotate(
+                is_success=Subquery(result_subquery)
+            ).distinct().order_by('infinitive'),
+        )
+
+        queryset = Table.objects.filter(
             Q(owner=self.request.user.profile)
             | Q(type='defaulttable')
-        )
+        ).prefetch_related(verbs_prefetch)
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return queryset.order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['default_tables'] = self.object_list.filter(
+
+        # # Subquery to get is_success for each Verb
+        # result_subquery = Result.objects.filter(
+        #     profile=self.request.user.profile,
+        #     table=OuterRef('tables'),
+        #     verb=OuterRef('pk')
+        # ).values('is_success')[:1]
+
+        # # Prefetch related verbs with the annotated is_success
+        # verbs_prefetch = Prefetch(
+        #     'verbs',
+        #     queryset=Verb.objects.annotate(
+        #         is_success=Subquery(result_subquery)
+        #     ).distinct().order_by('infinitive'),
+        # )
+
+        # # Query all default tables with the annotated verbs
+        # default_tables = DefaultTable.objects.prefetch_related(verbs_prefetch)
+
+        # # Query all user tables with the annotated verbs
+        # user_tables = UserTable.objects.filter(
+        #     owner=self.request.user.profile
+        # ).select_related('owner').prefetch_related(verbs_prefetch)
+
+        # context['default_tables'] = default_tables
+        # context['user_tables'] = user_tables
+
+        queryset = self.get_queryset()
+
+        context['default_tables'] = queryset.filter(
             type='defaulttable'
         )
-        context['user_tables'] = self.object_list.filter(
+        context['user_tables'] = queryset.filter(
             type='usertable'
         )
+
         return context
