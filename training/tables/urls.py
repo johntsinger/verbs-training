9a99e35840970db99b50588efd70675303bd7bc6
John <john.t.singer@gmail.com>
2024-10-08 15:57:03 +0200

    add table detail view, url, and template and add url to table detail in table list template

--- training/tables/urls.py
+++ training/tables/urls.py
@@ -5,5 +5,9 @@ from tables import views
 app_name = 'tables'
 urlpatterns = [
     path('', views.TableListView.as_view(), name='list'),
-    path('<int:pk>/', views.TableDetailView.as_view(), name='detail')
+    path(
+        '<uuid:pk>/<str:slug_name>/',
+        views.TableDetailView.as_view(),
+        name='detail'
+    )
 ]
--- training/tables/views.py
+++ training/tables/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q, Prefetch, Subquery, OuterRef
+from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-from common.views.mixins import TitleMixin
-from tables.models import Table, DefaultTable, UserTable
+from common.views.mixins import TitleMixin, PreviousPageURLMixin
+from tables.models import Table
 from results.models import Result
 from verbs.models import Verb
 
@@ -15,7 +17,7 @@ class TableListView(
     ListView
 ):
     model = Table
-    template_name = 'tables/tables_list.html'
+    template_name = 'tables/table_list.html'
     title = _('Tables')
 
     def get_queryset(self):
@@ -43,35 +45,7 @@ class TableListView(
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # # Subquery to get is_success for each Verb
-        # result_subquery = Result.objects.filter(
-        #     profile=self.request.user.profile,
-        #     table=OuterRef('tables'),
-        #     verb=OuterRef('pk')
-        # ).values('is_success')[:1]
-
-        # # Prefetch related verbs with the annotated is_success
-        # verbs_prefetch = Prefetch(
-        #     'verbs',
-        #     queryset=Verb.objects.annotate(
-        #         is_success=Subquery(result_subquery)
-        #     ).distinct().order_by('infinitive'),
-        # )
-
-        # # Query all default tables with the annotated verbs
-        # default_tables = DefaultTable.objects.prefetch_related(verbs_prefetch)
-
-        # # Query all user tables with the annotated verbs
-        # user_tables = UserTable.objects.filter(
-        #     owner=self.request.user.profile
-        # ).select_related('owner').prefetch_related(verbs_prefetch)
-
-        # context['default_tables'] = default_tables
-        # context['user_tables'] = user_tables
-
         queryset = self.get_queryset()
-
         context['default_tables'] = queryset.filter(
             type='defaulttable'
         )
@@ -80,3 +54,39 @@ class TableListView(
         )
 
         return context
+
+
+class TableDetailView(
+    TitleMixin,
+    PreviousPageURLMixin,
+    LoginRequiredMixin,
+    DetailView
+):
+    title = None
+    previous_page_url = reverse_lazy('tables:list')
+
+    def get_queryset(self):
+        table_id = self.kwargs.get(self.pk_url_kwarg)
+        # Subquery to get is_success for each Verb
+        result_subquery = Result.objects.filter(
+            profile=self.request.user.profile,
+            table_id=table_id,
+            verb=OuterRef('pk')
+        ).values('is_success')[:1]
+
+        # Prefetch related verbs with the annotated is_success
+        verbs_prefetch = Prefetch(
+            'verbs',
+            queryset=Verb.objects.annotate(
+                is_success=Subquery(result_subquery)
+            ).prefetch_related('info', 'examples').order_by('infinitive'),
+        )
+        queryset = Table.objects.filter(
+            Q(owner=self.request.user.profile)
+            | Q(type='defaulttable')
+        )
+
+        return queryset.prefetch_related(verbs_prefetch)
+
+    def get_title(self):
+        return self.object.name
