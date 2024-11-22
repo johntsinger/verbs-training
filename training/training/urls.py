c9f2c7f2812ac579e221235f2262756f064fc1c4
John <john.t.singer@gmail.com>
2024-11-22 14:40:52 +0100

    add js catalogue for FilrerdSelectMultiple widget

--- training/training/urls.py
+++ training/training/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.i18n import JavaScriptCatalog
+
+
+admin_urls = admin.site.urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('', include('authentication.urls')),
     path('verbs/', include('verbs.urls')),
     path('tables/', include('tables.urls')),
