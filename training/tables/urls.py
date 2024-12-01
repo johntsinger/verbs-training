7914dd1e987311f6477a527fb41c5b1083e7e7de
John <john.t.singer@gmail.com>
2024-12-01 23:38:21 +0100

    create two different urls patterns for default and user tables, add new urls for create, update and delete

--- training/tables/urls.py
+++ training/tables/urls.py
@@ -1,23 +1,77 @@
-from django.urls import path
+from django.urls import include, path
 from tables import views
 
 
 app_name = 'tables'
-urlpatterns = [
-    path('', views.TableListView.as_view(), name='list'),
+
+default_tables_urls = [
     path(
         '<uuid:pk>/<str:slug_name>/',
-        views.TableDetailView.as_view(),
+        views.DefaultTableDetailView.as_view(),
         name='detail'
     ),
     path(
+        'add/',
+        views.DefaultTableCreateView.as_view(),
+        name='add',
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/change/',
+        views.DefaultTableUpdateView.as_view(),
+        name='change'
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/delete/',
+        views.DefaultTableDeleteView.as_view(),
+        name='delete'
+    ),
+    path(
         '<uuid:pk>/<str:slug_name>/training/',
-        views.TrainingFormView.as_view(),
+        views.DefaultTableTrainingFormView.as_view(),
         name='training'
     ),
     path(
         '<uuid:pk>/<str:slug_name>/training/results/',
-        views.ResultView.as_view(),
+        views.DefaultTableResultView.as_view(),
         name='results'
-    )
+    ),
+]
+
+user_tables_urls = [
+    path(
+        '<uuid:pk>/<str:slug_name>/',
+        views.UserTableDetailView.as_view(),
+        name='detail'
+    ),
+    path(
+        'add/',
+        views.UserTableCreateView.as_view(),
+        name='add',
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/change/',
+        views.UserTableUpdateView.as_view(),
+        name='change'
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/delete/',
+        views.UserTableDeleteView.as_view(),
+        name='delete'
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/training/',
+        views.UserTableTrainingFormView.as_view(),
+        name='training'
+    ),
+    path(
+        '<uuid:pk>/<str:slug_name>/training/results/',
+        views.UserTableResultView.as_view(),
+        name='results'
+    ),
+]
+
+urlpatterns = [
+    path('', views.TableListView.as_view(), name='list'),
+    path('default/', include((default_tables_urls, 'default'))),
+    path('user/', include((user_tables_urls, 'user'))),
 ]
