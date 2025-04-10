32f094e2b32bd266d45d51edb583dfc995ae80a3
Jonathan Singer <john.t.singer@gmail.com>
2025-04-10 13:48:21 +0200

    add new breakpoint, create custom nav

--- training/static/js/slick-config.js
+++ training/static/js/slick-config.js
@@ -6,12 +6,22 @@
 
 var settings = {
     dots: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     infinite: false,
+    dotsClass: "slick-dots",
     responsive: [
         {
             breakpoint: 992,
+            settings: {
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {  
+            breakpoint: 476,
             settings: {
                 dots: true,
                 slidesToShow: 1,
@@ -24,7 +34,21 @@ var settings = {
     ]
 }
 
+var defaulttableSettings = {
+    ...settings,
+    appendDots: $("#default-tables .slider-controls .dots-container"),
+    prevArrow: $("#default-tables .slider-controls .prev-container .prev"),
+    nextArrow: $("#default-tables .slider-controls .next-container .next")
+}
+
+var usertableSettings = {
+    ...settings,
+    appendDots: $("#user-tables .slider-controls .dots-container"),
+    prevArrow: $("#user-tables .slider-controls .prev-container .prev"),
+    nextArrow: $("#user-tables .slider-controls .next-container .next")
+}
+
 $(document).ready(function(){
-    $('.default-tables__slick').slick(settings);
-    $('.user-tables__slick').slick(settings);
+    $('.default-tables__slick').slick(defaulttableSettings);
+    $('.user-tables__slick').slick(usertableSettings);
 });
